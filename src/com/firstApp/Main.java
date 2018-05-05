e872ae57295c1d3318768e82107478dd15201781
Tiago de Lima <tiago.blima@ufrpe.br>
2018-05-05 14:25:37 -0300

    First classes

--- src/com/firstApp/Main.java
+++ src/com/firstApp/Main.java
@@ -4,6 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-    
+        Cliente cliente = new Cliente("Tiago",18,1101010);
+        System.out.println(cliente.toString());
     }
 }
